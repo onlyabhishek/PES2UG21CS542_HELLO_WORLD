1512d58adaea431a730c9ed1566ee8a100ab81e6
onlyAbhishek <112745877+onlyabhishek@users.noreply.github.com>
2024-03-12 20:48:16 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, world! This is PES2UG21CS542" << std::endl;
+    std::cout << "Hello, world! This is PES2UG21CS542.This is SRINIVAASAN N S" << std::endl;
     return 0;
 }
